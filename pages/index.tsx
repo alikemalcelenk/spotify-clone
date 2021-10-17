015803424922d081a6a22a4923d8efda4cf5aba8
Ali Kemal Çelenk <alikemalcelenkk@gmail.com>
2021-10-17 20:51:31 +0300

    install svgr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import { FunctionComponent } from 'react'
 
+// icons
+import * as Icons from '../components/Icons'
+
 const HomePage: FunctionComponent = () => {
-  return <div className="bg-blue-100">Welcome to Next.js!</div>
+  return (
+    <div className="bg-blue-100">
+      <p>Welcome to Next.js!</p>
+      <Icons.Home className="text-blue-500" />
+    </div>
+  )
 }
 
 export default HomePage
